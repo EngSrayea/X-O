bf1abd8a42cb7cc5e7afe01bfd5f9690bc55e2f8
EngSrayea <eng.nsrayea@gmail.com>
2020-05-09 22:26:05 +0300

    Initial Commit

---
+++ X-O/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X-O
+//
+//  Created by Eng. Norii on 5/9/20.
+//  Copyright © 2020 EngSrayea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
